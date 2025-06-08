3f6c4cb3c66826a900e061efe369578088055c8b
Saurabh Agarwal <saurabh1434@gmail.com>
2025-06-08 13:24:36 +0530

    Testing

--- Scripts/copy_build_files.sh
+++ Scripts/copy_build_files.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Copying build files to /var/www/html"
-cp -r /tmp/codedeploy-deployment-*/deployment-archive/build/* /var/www/html/
+
+# Get the actual deployment root path
+DEPLOY_DIR=$(dirname "$(readlink -f "$0")")/..
+
+# Clear existing files (optional)
+sudo rm -rf /var/www/html/*
+
+# Copy build output
+sudo cp -r "$DEPLOY_DIR/build/"* /var/www/html/
