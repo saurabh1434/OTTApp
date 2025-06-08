19544a7522b73aa85ec2e91994d5e55ca14e188a
Saurabh Agarwal <saurabh1434@gmail.com>
2025-06-08 12:48:17 +0530

    TestCommit

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -2,4 +2,5 @@ export const banners = [
     { id: 1, title: "Alladin's Love", image: "/banner1.jpeg", file: "Test Teaser.mp4" },
     { id: 2, title: "When we Love in deserts", image: "/banner2.jpeg", file: "Test Video.mp4" },
     { id: 3, title: "Fire", image: "/banner3.jpeg", file: "BannerVideo1.mp4" },
+    
   ];
